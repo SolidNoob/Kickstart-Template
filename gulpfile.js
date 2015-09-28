2bca417a9544968e76a5c31efa4f373425904b7f
SolidNoob <anthony.cammarata@hotmail.com>
2015-09-29 01:30:49 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+//npm install --save-dev gulp
+//npm install --save-dev gulp-concat-css gulp-jshint gulp-sass gulp-concat gulp-concat-css gulp-uglify gulp-rename gulp-minify-css
+
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('resources/js/main.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('resources/scss/app.scss')
+        .pipe(sass())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest('resources/css'));
+});
+
+// Concatenate & Minify CSS Files
+gulp.task('styles', ['sass'], function () {
+    return gulp.src([
+        'resources/css/libs/*.css', 
+        'resources/css/app.css'
+        ])
+    .pipe(concatCss('app.min.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('public/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src([
+        'resources/js/libs/jquery.min.js', 
+        'resources/js/libs/bootstrap.min.js', 
+        //'resources/js/libs/npm.js',
+        'resources/js/app.js'
+        ])
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(uglify())
+});
+
+
+// Watch Files For Changes
+
+gulp.task('watch', function() {
+    gulp.watch('resources/js/*.js', ['lint', 'scripts']);
+    gulp.watch('resources/scss/*.scss', ['sass', 'styles']);
+});
+
+
+
+gulp.task('default', ['lint', 'styles', 'scripts', 'watch']);
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kickstart Template</title>
+	
+  	<!-- Font(s) -->
+  	<link href='https://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
+
+    <!-- Stylesheet(s) -->
+    <link href="public/css/app.min.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav id="main-navbar" class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Kickstart Template</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#about">FAQ</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#blog">Blog</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>Hello world!</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae blanditiis in, illo rerum tenetur eligendi accusamus maiores aliquid sed, dolorem dolor vel error magnam saepe nisi reprehenderit temporibus ipsa voluptate.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Js File(s) -->
+    <script src="public/js/app.min.js"></script>
+  </body>
+</html>
